3cea7e784ef68a8ff0237bdc8c7358e2eb63d686
Yuriy Ivanov <yura0ivanov@gmail.com>
2023-01-08 17:18:34 +0200

    adds HW #15

--- app.js
+++ app.js
@@ -1,32 +1,75 @@
-let str1 = "pappy";
-let str2 = "apple";
-function coloringString(str1, str2) {
-  if (str1.length !== str2.length)
-    throw Error("ERROR! Not equal length of strings");
-  const arr1 = Array.from(str1.toLowerCase());
-  const arr2 = Array.from(str2.toLowerCase());
-  const res = arr2.map(myFunction);
-  function myFunction(item, index) {
-    if (arr1.includes(item) && item !== arr1[index]) item = "yellow";
-    else if (item == arr1[index]) item = "green";
-    else item = "red";
-    return item;
-  }
-  return res;
-}
-console.log(coloringString(str1, str2));
-console.log(coloringString(str1, str1));
-
-const arr1 = [1, 100, -100, 25, 1000];
-
-function getNumbersWithDigitsAmount(digitsAmount, array) {
-  if (digitsAmount < 1) throw Error("ERROR! Not acceptable amount of digits");
-  const min = 10 ** (digitsAmount - 1);
-  const max = 10 ** digitsAmount - 1;
-  const res = array.filter(function (number) {
-    number = Math.abs(number);
-    return number >= min && number <= max;
+const arr1 = [20, -10, 333, 1000, 552, 7, -7];
+function evenOddSort(array) {
+  array.sort(function (a, b) {
+    return (Math.abs(b) % 2) - (Math.abs(a) % 2);
   });
-  return res;
+  return array;
 }
-console.log(getNumbersWithDigitsAmount(3, arr1));
+console.log(evenOddSort(arr1));
+
+const arr2 = [20, -10, 333, 1000, 552, 7, -7];
+function oddEvenSort(array) {
+  array.sort(function (a, b) {
+    return (Math.abs(a) % 2) - (Math.abs(b) % 2);
+  });
+  return array;
+}
+console.log(oddEvenSort(arr2));
+
+const arr3 = [20, -10, 333, 1000, 552, 7, -7];
+function evenAscOddDesc(array) {
+  array.sort(function (a, b) {
+    return (Math.abs(a) % 2) - (Math.abs(b) % 2) || (a % 2 ? b - a : a - b);
+  });
+  return array;
+}
+console.log(evenAscOddDesc(arr3));
+
+const arr4 = [20, -10, 333, 1000, 552, 7, -7];
+function getMin(array) {
+  const result = array.reduce(function (res, curr) {
+    return curr < res ? curr : res;
+  });
+  return result;
+}
+console.log("************************************");
+console.log(getMin(arr4));
+
+const arr5 = [20, -10, 333, 1000, 552, 7, -7];
+function getMax(array) {
+  const result = array.reduce(function (res, curr) {
+    return curr > res ? curr : res;
+  });
+  return result;
+}
+console.log(getMax(arr5));
+
+const arr6 = [20, -10, 333, 1000, 552, 7, -7];
+function getAverage(array) {
+  const result =
+    array.reduce(function (res, curr) {
+      return res + curr;
+    }) / array.length;
+  return result;
+}
+console.log(getAverage(arr6));
+
+const arr7 = [20, -10, 333, 1000, 552, 7, -7];
+function getMinMaxAvg(array) {
+  const result = array.reduce(
+    function (res, curr) {
+      if (curr < res[0]) res[0] = curr;
+      else if (curr > res[1]) res[1] = curr;
+      res[2] += curr / array.length;
+      return res;
+    },
+    [array[0], 0, 0]
+  );
+  return result;
+}
+console.log("********************************");
+console.log(
+  `min value: ${getMinMaxAvg(arr7)[0]} \nmax value: ${
+    getMinMaxAvg(arr7)[1]
+  } \naverage value: ${getMinMaxAvg(arr7)[2]}`
+);
